68ef2aa52f7b6c100fa2d69f0b835392eedab742
scottsc0de <scott5902@gmail.com>
2022-10-18 07:51:09 -0400

    added please enter event message

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,44 +36,43 @@ $(function () {
             currentBlock.removeClass("present");
             currentBlock.addClass("future");
         }
+    });
 
-        // clicking save event button adds to local storage and shows confirmation message
-        // .on() the shortened addEventListener "onclick"
-        saveEvents.on("click", function () {
-            // .siblings(), targetting the save buttons sibling elements
-            var timeBlockInput = $(this).siblings(".time-block")
-            var userText = timeBlockInput.val();
-            var savedEvent = parseInt(timeBlockInput.attr("value"));
-            localStorage.setItem(savedEvent, JSON.stringify(userText)); // creating local storage key & value
-            if (userText !== null) {
-                confirmationMsg.text("Event saved to local storage✅");
-                confirmationMsg.css('display', 'block');
-            } else {
-                confirmationMsg.text("Please enter an event!");
-                confirmationMsg.css('display', 'block');
-            }
-        });
-
-        // user input stays in box after refresh
-        // grabbing each time block seperately by their local storage key 
-        // and displaying their local storage value to the input box
-        $("#9").val(JSON.parse(localStorage.getItem('9')));
-        $("#10").val(JSON.parse(localStorage.getItem('10')));
-        $("#11").val(JSON.parse(localStorage.getItem('11')));
-        $("#12").val(JSON.parse(localStorage.getItem('12')));
-        $("#13").val(JSON.parse(localStorage.getItem('1')));
-        $("#14").val(JSON.parse(localStorage.getItem('2')));
-        $("#15").val(JSON.parse(localStorage.getItem('3')));
-        $("#16").val(JSON.parse(localStorage.getItem('4')));
-        $("#17").val(JSON.parse(localStorage.getItem('5')));
-
-        // a button for clearing event text inside input boxes
-        clearButton.on("click", function () {
-            localStorage.clear();
-            confirmationMsg.text('Events cleared from local storage❌');
+    // clicking save event button adds to local storage and shows confirmation message
+    // .on() the shortened addEventListener "onclick"
+    saveEvents.on("click", function () {
+        // .siblings(), targetting the save buttons sibling elements
+        var timeBlockInput = $(this).siblings(".time-block")
+        var userText = timeBlockInput.val();
+        var savedEvent = parseInt(timeBlockInput.attr("value"));
+        localStorage.setItem(savedEvent, JSON.stringify(userText)); // creating local storage key & value
+        if (userText) {
+            confirmationMsg.text("Event saved to local storage✅");
             confirmationMsg.css('display', 'block');
-            timeBlocks.val("");
-        })
+        } else {
+            confirmationMsg.text("Please enter an event!");
+            confirmationMsg.css('display', 'block');
+        }
+    });
 
+    // user input stays in box after refresh
+    // grabbing each time block seperately by their local storage key 
+    // and displaying their local storage value to the input box
+    $("#9").val(JSON.parse(localStorage.getItem('9')));
+    $("#10").val(JSON.parse(localStorage.getItem('10')));
+    $("#11").val(JSON.parse(localStorage.getItem('11')));
+    $("#12").val(JSON.parse(localStorage.getItem('12')));
+    $("#13").val(JSON.parse(localStorage.getItem('1')));
+    $("#14").val(JSON.parse(localStorage.getItem('2')));
+    $("#15").val(JSON.parse(localStorage.getItem('3')));
+    $("#16").val(JSON.parse(localStorage.getItem('4')));
+    $("#17").val(JSON.parse(localStorage.getItem('5')));
+
+    // a button for clearing event text inside input boxes
+    clearButton.on("click", function () {
+        localStorage.clear();
+        confirmationMsg.text('Events cleared from local storage❌');
+        confirmationMsg.css('display', 'block');
+        timeBlocks.val("");
     })
 });
