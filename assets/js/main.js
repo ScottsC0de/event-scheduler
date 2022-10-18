5fbc0474660b2036ff2bb574b96accd1c74996bd
scottsc0de <scott5902@gmail.com>
2022-10-18 07:22:03 -0400

    added current date and time using moment.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,8 @@ var timeBlocks = $(".time-block");
 var saveEvents = $(".saveBtn");
 var confirmationMsg = $("#confirmation");
 var clearButton = $("#clearBtn");
+
+// date and current time up top
+// will need to reference this for time block color
+var currentDateAndTime = moment();
+$("#currentDay").text(currentDateAndTime.format("dddd MMM Do, YYYY h:mma"));
