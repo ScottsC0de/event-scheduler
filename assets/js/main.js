d4cb5b563ea5962d61576a7fd4db924c72caf310
scottsc0de <scott5902@gmail.com>
2022-10-18 07:20:49 -0400

    targetted HTML elements with jQuery

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+// target HTML elements using jQuery
+var timeBlocks = $(".time-block");
+var saveEvents = $(".saveBtn");
+var confirmationMsg = $("#confirmation");
+var clearButton = $("#clearBtn");
